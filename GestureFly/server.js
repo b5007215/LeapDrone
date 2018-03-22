8d60365f18f2af99c01c954a2f2dc67b8c51279c
Mohammed Yafai <b5007215@my.shu.ac.uk>
2018-03-22 03:27:21 +0000

    Added backflip

--- GestureFly/server.js
+++ GestureFly/server.js
@@ -56,7 +56,7 @@ io.sockets.on('connection', function (socket) {
 		else if (type === 'flip') {
 			console.log('flip');
 			if (flight) {
-				//runAction(client.backFlip);
+				runAction(client.animate, ['flipBehind', 150]);
 			}
 		}
 		// Move drone forward
